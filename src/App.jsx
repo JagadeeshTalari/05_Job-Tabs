79bf8b68929ce4911eab43b6662693a2eb0ae209
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2024-02-02 18:10:12 +0530

    total functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuid } from "uuid";
 
 const url = "https://course-api.com/react-tabs-project";
 
@@ -6,6 +8,8 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+
+  //allJobs
   useEffect(() => {
     const fetchJobs = async () => {
       setIsLoading(true);
@@ -16,63 +20,49 @@ const App = () => {
       setIsLoading(false);
     };
     fetchJobs();
-
-    // company: "TOMMY";
-    // dates: "December 2015 - Present";
-    // duties: (3)[
-    //   ("Tote bag sartorial mlkshk air plant vinyl banjo lu…ch neutra. Hammock photo booth live-edge disrupt.",
-    //   "Post-ironic selvage chambray sartorial freegan med… toast street art cloud bread live-edge heirloom.",
-    //   "Butcher drinking vinegar franzen authentic messeng…ffee franzen cloud bread tilde vegan flexitarian.")
-    // ];
-    // id: "recAGJfiU4CeaV0HL";
-    // order: 3;
-    // title: "Full Stack Web Developer";
   }, []);
 
-  const activeJob = (id) => {
-    const newIndex = jobs.filter((job) => {
-      if (job.id === id) {
-        return setIndex(jobs.indexOf(job));
-      }
-    });
-  };
-
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>;
+      </section>
+    );
   }
 
   return (
     <main>
-      <section className="btn-container">
-        {jobs.map((job) => {
-          return (
-            <button
-              key={job.id}
-              className="job-btn"
-              onClick={() => activeJob(job.id)}
-            >
-              {job.company}
-            </button>
-          );
-        })}
-      </section>
-      <h1>{jobs[index].title}</h1>
-      <section className="jobs-center">
-        <div className="job-company">{jobs[index].company}</div>
-        <div className="job-date">{jobs[index].dates}</div>
-        <div className="job-desc">
-          <article>
-            {jobs[index].duties.map((duty, i) => {
+      <div className="jobs-center">
+        <section className="btn-container">
+          {jobs.map((job, i) => {
+            return (
+              <button
+                key={job.id}
+                className={`job-btn ${i === index ? "active-btn" : null}`}
+                onClick={() => setIndex(i)}
+              >
+                {job.company}
+              </button>
+            );
+          })}
+        </section>
+
+        <article className="jobs-info">
+          <h3>{jobs[index].title}</h3>
+          <div className="job-company">{jobs[index].company}</div>
+          <div className="job-date">{jobs[index].dates}</div>
+          <div>
+            {jobs[index].duties.map((duty) => {
               return (
-                <div key={i}>
-                  <span className="job-icon">""</span>
+                <div key={uuid()} className="job-desc">
+                  <FaAngleDoubleRight className="job-icon" />
                   <p>{duty}</p>
                 </div>
               );
             })}
-          </article>
-        </div>
-      </section>
+          </div>
+        </article>
+      </div>
     </main>
   );
 };
